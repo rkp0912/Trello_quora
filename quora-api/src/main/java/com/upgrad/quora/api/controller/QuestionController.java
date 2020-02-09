f7220964426908e300a3bb387db3d161480ac5ad
E532650 <Krishna.Praveen@Honeywell.com>
2020-02-09 08:09:58 +0530

    Edit question content

--- quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -1,19 +1,15 @@
 package com.upgrad.quora.api.controller;
 
-import com.upgrad.quora.api.model.QuestionDetailsResponse;
-import com.upgrad.quora.api.model.QuestionRequest;
-import com.upgrad.quora.api.model.QuestionResponse;
+import com.upgrad.quora.api.model.*;
 import com.upgrad.quora.service.business.QuestionBusinessService;
 import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.InvalidQuestionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
@@ -75,5 +71,22 @@ public class QuestionController {
         return new ResponseEntity<List<QuestionDetailsResponse>>(questionDetailsResponseArrayList, HttpStatus.OK);
     }
 
+
+    @RequestMapping(method = RequestMethod.PUT, path="/question/edit/{questionId}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+    produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<QuestionEditResponse> editQuestionContent(
+            final QuestionEditRequest questionEditRequest,
+            @PathVariable("questionId") String uuid,
+            @RequestHeader("authorization") final String authorization)
+            throws AuthorizationFailedException, InvalidQuestionException{
+
+        final QuestionEntity questionEntity = new QuestionEntity();
+        questionEntity.setUuid(uuid);
+        questionEntity.setContent(questionEditRequest.getContent());
+        QuestionEntity updatedQuestion = questionBusinessService.editQuestionContent(questionEntity, authorization);
+        QuestionEditResponse questionEditResponse = new QuestionEditResponse().id(updatedQuestion.getUuid())
+                .status("QUESTION EDITED");
+        return new ResponseEntity<QuestionEditResponse>(questionEditResponse, HttpStatus.OK);
+    }
 
 }
--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
@@ -6,6 +6,7 @@ import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.InvalidQuestionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -67,6 +68,43 @@ public class QuestionBusinessService {
                 throw new AuthorizationFailedException("ATHR-002","User is signed out.Sign in first to get all questions");
             } else{
                 return questionDao.getAllQuestions();
+            }
+        }
+    }
+
+
+    /**
+     * Method checks if the authorization token is valid, checks if the question with uuid exits.
+     * If success then update the question details in the DB
+     * @param updatedQuestion
+     * @param authorizationToken
+     * @return
+     * @throws AuthorizationFailedException
+     * @throws InvalidQuestionException
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public QuestionEntity editQuestionContent(
+            QuestionEntity updatedQuestion, final String authorizationToken)
+            throws AuthorizationFailedException, InvalidQuestionException{
+
+        UserAuthEntity userAuthEntity = userDao.getUserAuth(authorizationToken);
+        if(userAuthEntity == null){
+            throw new AuthorizationFailedException("ATHR-001","User has not signed in");
+        } else{
+            if(userAuthEntity.getLogoutAt() != null){
+                throw new AuthorizationFailedException("ATHR-002","User is signed out.Sign in first to edit the question");
+            } else{
+                    QuestionEntity questionEntity = questionDao.getQuestionByUuid(updatedQuestion.getUuid());
+                    if(questionEntity == null){
+                        throw new InvalidQuestionException("QUES-001","Entered question uuid does not exist");
+                    }
+                    if(questionEntity.getUser().getId() == userAuthEntity.getUser().getId()){
+                        questionEntity.setContent(updatedQuestion.getContent());
+                        questionDao.updateQuestion(questionEntity);
+                        return questionEntity;
+                    } else {
+                        throw new AuthorizationFailedException("ATHR-003","Only the question owner can edit the question");
+                    }
             }
         }
     }
